45b8bd63a6632da60a02d7f708c8f5033d2ec1c7
suhtoshi <haydenk.lewis@gmail.com>
2021-06-11 13:12:20 -0500

    added connection with cloud database

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
 
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageUrl', {
+      fetch('https://frozen-forest-65336.herokuapp.com/imageUrl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://frozen-forest-65336.herokuapp.com/:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
         this.setState({password: event.target.value})
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://frozen-forest-65336.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
